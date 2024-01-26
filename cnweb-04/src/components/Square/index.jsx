93a218d49deba568968c4c149f767dc9b4e3f772
tuva <tuva@thanglong.edu.vn>
2024-01-26 14:20:02 +0700

    Buổi 13: Board and square

---
+++ cnweb-04/src/components/Square/index.jsx
@@ -0,0 +1,8 @@
+import "./style.css"
+const Square = function () {
+    return <div className="square">
+        x
+    </div>
+}
+
+export default Square
--- cnweb-04/src/components/UserProfile/index.jsx
+++ cnweb-04/src/components/UserProfile/index.jsx
@@ -2,15 +2,9 @@ import { useState } from "react"
 import "./style.css"
 
 function UserProfile(props) {
-    const state = { active: true } // Biến - Variable
-    const [active, setActive] = useState(false) // State
-    const changeActive = function () {
-        setActive(!active)
-    }
     return <div>
-        <img className={active ? "user-image active" : "user-image"} src={props.image} alt="" />
+        <img className={props.active ? "user-image active" : "user-image"} src={props.image} alt="" />
         <p>{props.name}</p>
-        <button onClick={changeActive}>Click me</button>
     </div>
 }
 
